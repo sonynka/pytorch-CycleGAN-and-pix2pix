ac0bac8ff0d41ce8c4b646c4806573d2f6f74b93
Sona Pecenakova <sona.pecenakova@gmail.com>
2018-07-22 18:12:32 +0200

    edit combining of pictures

--- datasets/combine_A_and_B.py
+++ datasets/combine_A_and_B.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import cv2
+from PIL import Image
 import argparse
 
 parser = argparse.ArgumentParser('create image pairs')
@@ -14,35 +14,42 @@ args = parser.parse_args()
 for arg in vars(args):
     print('[%s] = ' % arg,  getattr(args, arg))
 
-splits = os.listdir(args.fold_A)
+img_files = os.listdir(args.fold_A)
+num_imgs = len(img_files)
+valid_AB_imgs = []
 
-for sp in splits:
-    img_fold_A = os.path.join(args.fold_A, sp)
-    img_fold_B = os.path.join(args.fold_B, sp)
-    img_list = os.listdir(img_fold_A)
+for n in range(num_imgs):
+    name_A = img_files[n]
+    path_A = os.path.join(args.fold_A, name_A)
     if args.use_AB:
-        img_list = [img_path for img_path in img_list if '_A.' in img_path]
+        name_B = name_A.replace('_A.', '_B.')
+    else:
+        name_B = name_A
+    path_B = os.path.join(args.fold_B, name_B)
 
-    num_imgs = min(args.num_imgs, len(img_list))
-    print('split = %s, use %d/%d images' % (sp, num_imgs, len(img_list)))
-    img_fold_AB = os.path.join(args.fold_AB, sp)
-    if not os.path.isdir(img_fold_AB):
-        os.makedirs(img_fold_AB)
-    print('split = %s, number of images = %d' % (sp, num_imgs))
-    for n in range(num_imgs):
-        name_A = img_list[n]
-        path_A = os.path.join(img_fold_A, name_A)
+    if os.path.isfile(path_A) and os.path.isfile(path_B):
+        name_AB = name_A
         if args.use_AB:
-            name_B = name_A.replace('_A.', '_B.')
-        else:
-            name_B = name_A
-        path_B = os.path.join(img_fold_B, name_B)
-        if os.path.isfile(path_A) and os.path.isfile(path_B):
-            name_AB = name_A
-            if args.use_AB:
-                name_AB = name_AB.replace('_A.', '.') # remove _A
-            path_AB = os.path.join(img_fold_AB, name_AB)
-            im_A = cv2.imread(path_A, cv2.CV_LOAD_IMAGE_COLOR)
-            im_B = cv2.imread(path_B, cv2.CV_LOAD_IMAGE_COLOR)
-            im_AB = np.concatenate([im_A, im_B], 1)
-            cv2.imwrite(path_AB, im_AB)
+            name_AB = name_AB.replace('_A.', '.') # remove _A
+        valid_AB_imgs.append(name_AB)
+
+num_valid_imgs = len(valid_AB_imgs)
+train_idx = int(num_valid_imgs*0.8)
+valid_idx = int(num_valid_imgs*0.9)
+splits = {'train': valid_AB_imgs[:train_idx],
+          'valid': valid_AB_imgs[train_idx:valid_idx],
+          'test': valid_AB_imgs[valid_idx:]}
+
+for split, split_files in splits.items():
+    folder_AB = os.path.join(args.fold_AB, split)
+    if not os.path.exists(folder_AB):
+        os.makedirs(folder_AB)
+
+    for img_name in split_files:
+        path_AB = os.path.join(folder_AB, img_name)
+        path_A = os.path.join(args.fold_A, img_name)
+        path_B = os.path.join(args.fold_B, img_name)
+        im_A = Image.open(path_A)
+        im_B = Image.open(path_B)
+        im_AB = np.concatenate([im_A, im_B], 1)
+        Image.fromarray(im_AB).save(path_AB)
